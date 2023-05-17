e7d9787991759857577a7cc1d7da4cf0a2e362d3
akshit30 <singhakshit2003@gmail.com>
2023-05-17 14:04:16 +0530

    added weatherapi functions

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.security.identity.IdentityCredentialException;
+import android.util.Log;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +38,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageView backIV,searchIV,iconIV;
     private ArrayList<WeatherRVModal> weatherRVModalArrayList;
     private WeatherRVAdapter weatherRVAdapter;
-
+    private LocationManager locationManager;
+    private int PERMISSION_CODE=1;
+    private String cityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
         homeRL=findViewById(R.id.idRlHome);
         loadingPB=findViewById(R.id.idPBLoading);
@@ -42,11 +60,44 @@ public class MainActivity extends AppCompatActivity {
         weatherRVModalArrayList = new ArrayList<>();
         weatherRVAdapter = new WeatherRVAdapter(this,weatherRVModalArrayList);
         weatherRV.setAdapter(weatherRVAdapter);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE);
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        cityName=getCityName(location.getLongitude(), location.getLatitude());
+        getWeatherInfo(cityName);
+
+
+    }
+    private String getCityName(double longitude, double latitude)
+    {
+        String cityName = "Not Found";
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try{
+            List<Address> addresses = gcd.getFromLocation(latitude,longitude,10);
+            for(Address adr: addresses){
+                if(adr!=null)
+                {
+                    String city= adr.getLocality();
+                    if(city!=null && !city.equals("")){
+                        cityName=city;
+                    }else{
+                        Log.d("TAG","City Not Found");
+                        Toast.makeText(this,"User city not found",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return cityName;
+
     }
 
     private void getWeatherInfo(String cityName)
     {
-
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=adf97523983d490eaa065136231705&q=" + cityName + "&days=1&aqi=yes&alerts=yes";
     }
 
 }
